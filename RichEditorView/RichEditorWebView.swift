4d2c3f0ae66c182be09a476ff8aa8f192658b587
MeetDoshi <meet.doshi@tullyapp.com>
2019-11-22 12:28:04 +0530

    pod updated

---
+++ RichEditorView/RichEditorWebView.swift
@@ -0,0 +1,24 @@
+//
+//  RichEditorWebView.swift
+//  RichEditorView
+//
+//  Created by admin on 22/11/19.
+//
+
+import WebKit
+
+public class RichEditorWebView: WKWebView {
+
+    public var accessoryView: UIView?
+    public var inPutViewView: UIView?
+
+    public override var inputAccessoryView: UIView? {
+        return accessoryView
+    }
+    
+    
+    public override var inputView: UIView? {
+        return inPutViewView
+    }
+
+}

42701ee1bce3472855e42c9e2e370078137825d0
meetdoshi1 <meet.doshi@tullyapp.com>
2020-07-20 16:02:17 +0530

    commented unwanted code

--- RichEditorView/Classes/RichEditorView.swift
+++ RichEditorView/Classes/RichEditorView.swift
@@ -668,11 +668,12 @@ private let DefaultInnerLineHeight: Int = 28
     /// - parameter method: String with the name of the method and optional parameters that were passed in
     private func performCommand(_ method: String) {
         //Still while loop
-        if method.contains("1pasteHtmlAtCaretCallBack"){
-            print("method",method)
-        }else if method.hasPrefix("Still while loop") {
-            print("Still while loop")
-        }else if method.hasPrefix("ready") {
+//        if method.contains("1pasteHtmlAtCaretCallBack"){
+//            print("method",method)
+//        }else if method.hasPrefix("Still while loop") {
+//            print("Still while loop")
+//        }else
+        if method.hasPrefix("ready") {
            // If loading for the first time, we have to set the content HTML to be displayed
            if !isEditorLoaded {
                 isEditorLoaded = true
@@ -707,7 +708,7 @@ private let DefaultInnerLineHeight: Int = 28
                 }
             }
             runJS("RE.getCursrPositionValue()") { r in
-                print("getCursrPositionValue,",r,"End")
+//                print("getCursrPositionValue,",r,"End")
                 self.delegate?.getRhymeWord?(r)
             }
 
@@ -739,7 +740,7 @@ private let DefaultInnerLineHeight: Int = 28
             delegate?.textCopied?(self)
         }else{
             runJS("RE.getCursrPositionValue()") { r in
-                print("getCursrPositionValue,",r,"End")
+//                print("getCursrPositionValue,",r,"End")
                 self.delegate?.getRhymeWord?(r)
             }
             self.checkEvents()
